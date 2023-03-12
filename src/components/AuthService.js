948ce9400695ac03a7d96543fc7c62c7825ed1c4
marcusbdtf <marcus.hedlund@hotmail.com>
2023-03-12 22:06:52 +0100

    test again

--- src/components/AuthService.js
+++ src/components/AuthService.js
@@ -1,36 +1,63 @@
-import * as Msal from '@azure/msal-browser';
+import * as msal from "@azure/msal-browser";
 
 export default class AuthService {
   constructor() {
     const config = {
       auth: {
         clientId: process.env.REACT_APP_CLIENT_ID,
-        authority: `https://login.microsoftonline.com/${process.env.REACT_APP_AUTHORITY}`,
       },
     };
 
-    this.myMsal = new Msal.PublicClientApplication(config);
+    this.myMsal = new msal.PublicClientApplication(config);
     this.request = {
       scopes: ['user.read'],
     };
 
-    this.idToken = '';
-    this.accessToken = '';
+    this.idToken = "";
+    this.accessToken = "";
   }
 
   login = async () => {
     try {
-      const pkceParams = {
-        codeChallengeMethod: 'S256', // use SHA-256 hashing for code challenge
-      };
-      const response = await this.myMsal.loginPopup({
-        ...this.request,
-        authenticationParameters: pkceParams,
-      });
+      const response = await this.myMsal.loginPopup(this.request);
       this.idToken = response.idToken;
       this.accessToken = response.accessToken;
     } catch (error) {
       console.log(error);
+    }
+  };
+
+  logout = async () => {
+    await this.myMsal.logout();
+  };
+
+  getToken = async () => {
+    try {
+      const accounts = this.myMsal.getAllAccounts();
+
+      if (accounts.length === 0) throw new Error("login_required");
+
+      const silentRequest = {
+        scopes: this.request.scopes,
+        account: accounts[0],
+      };
+
+      const authResult = await this.myMsal.acquireTokenSilent(silentRequest);
+
+      return authResult.accessToken;
+    } catch (error) {
+      if (error instanceof msal.InteractionRequiredAuthError) {
+        const interactiveRequest = {
+          scopes: this.request.scopes,
+        };
+
+        const authResult = await this.myMsal.acquireTokenPopup(interactiveRequest);
+
+        return authResult.accessToken;
+      } else {
+        console.log(error);
+        throw error;
+      }
     }
   };
 
