57ea4b3e9ea5f4bdf9e04bd03fd5465f4f5f4543
marcusbdtf <marcus.hedlund@hotmail.com>
2023-03-12 16:55:47 +0100

    PKCE

--- src/index.js
+++ src/index.js
@@ -3,13 +3,21 @@ import ReactDOM from 'react-dom/client';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 import App from './App';
-import { PublicClientApplication, EventType } from '@azure/msal-browser';
+import { PublicClientApplication, EventType, CryptoUtils } from '@azure/msal-browser';
+
+const codeVerifier = CryptoUtils.generateCodeVerifier(); // generate a random string
+const codeChallenge = CryptoUtils.generateCodeChallenge(codeVerifier); // generate the corresponding challenge string
 
 export const pca = new PublicClientApplication({
   auth:{
     clientId: 'ff534cd6-bff6-46af-83e5-48211dd64e13',
     authority: 'https://login.microsoftonline.com/common/',
     redirectUri: 'https://reactwebapp-mh.azurewebsites.net/.auth/login/aad/callback',
+    codeChallenge,
+    codeChallengeMethod: 'S256',
+    extraQueryParameters: {
+      code_verifier: codeVerifier 
+    }
   },
   cache: {
     cacheLocation: 'localStorage',
@@ -38,5 +46,3 @@ root.render(
     <App msalInstance={pca}/>
   </React.StrictMode>
 );
-
-
