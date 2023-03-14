4ff2a151c6ca2b5b1ccd91e3a46c96d9e63c5841
marcusbdtf <marcus.hedlund@hotmail.com>
2023-03-14 17:19:31 +0100

    authflow upd

--- src/components/AuthService.js
+++ src/components/AuthService.js
@@ -7,7 +7,8 @@ export default class AuthService {
         clientId: "bc05b57c-a494-4023-baa7-548011196461",
         authority: `https://login.microsoftonline.com/5b679921-53f7-4642-a251-8a603608d21c`,
         redirectUri: "https://reactwebapp-mh.azurewebsites.net/.auth/login/aad/callback",
-        responseType: "id_token",
+        responseType: "code",
+        codeChallengeMethod: "S256", 
       },
       cache: {
         cacheLocation: 'localStorage',
@@ -16,6 +17,7 @@ export default class AuthService {
         iframeTimeout: 6000
       }
     };
+    
     
 
     this.myMsal = new PublicClientApplication(config);
